58fba5eadc3cff1136dadb0907cb6e32a7d1bce5
Josh Bohde <josh@joshbohde.com>
2016-03-09 19:46:26 -0600

    Better modeling of push, with Links added.

--- src/Pushbullet/Models/PushRequest.hs
+++ src/Pushbullet/Models/PushRequest.hs
@@ -1,21 +1,85 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DeriveGeneric       #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Pushbullet.Models.PushRequest (
+  Target(..),
+  Message(..),
   PushRequest(..)
   ) where
 
-import           Data.Aeson.TH          (defaultOptions, deriveJSON,
-                                         fieldLabelModifier)
+import           Control.Applicative    ((<$>), (<*>), (<|>))
+import           Data.Aeson             (FromJSON, ToJSON, object, parseJSON,
+                                         toJSON, (.:), (.:?), (.=))
+import qualified Data.Aeson.Types       as A
+import           Data.Monoid            ((<>))
 import qualified Data.Text              as T
+import           GHC.Generics           (Generic)
 import           Pushbullet.Models.Push (PushType)
 
-data PushRequest = PushRequest {
-  device_iden :: Maybe T.Text,
-  email       :: Maybe T.Text,
-  channel_tag :: Maybe T.Text,
-  client_iden :: Maybe T.Text,
-  push_type   :: PushType,
-  title       :: T.Text,
-  body        :: T.Text
-  } deriving (Show, Eq)
+data Target = All
+            | Device T.Text
+            | Email T.Text
+            | Channel T.Text
+            | Client T.Text
+            deriving (Show, Eq, Generic)
 
-$(deriveJSON defaultOptions{fieldLabelModifier = (\n -> if n == "push_type" then "type" else n)} ''PushRequest)
+
+targetPairs :: Target -> [A.Pair]
+targetPairs All = []
+targetPairs (Device t) = ["device_id" .= t]
+targetPairs (Email t) = ["email" .= t]
+targetPairs (Channel t) = ["channel_tag" .= t]
+targetPairs (Client t) = ["client_iden" .= t]
+
+instance ToJSON Target where
+  toJSON = object . targetPairs
+
+instance FromJSON Target where
+  parseJSON (A.Object v) =     (Device <?> "device_id")
+                           <|> (Email <?> "email")
+                           <|> (Channel <?> "channel_tag")
+                           <|> (Client <?> "client_iden")
+                           <|> pure All
+    where
+      f <?> k = f <$> v .: k
+  parseJSON invalid = A.typeMismatch "Target" invalid
+
+
+
+data Message = Note (Maybe T.Text) (Maybe T.Text)
+             | Link (Maybe T.Text) (Maybe T.Text) (Maybe T.Text)
+             deriving (Show, Eq, Generic)
+
+messagePairs :: Message -> [A.Pair]
+messagePairs (Note title body) = [
+    "type" .= ("note" :: T.Text),
+    "title" .= title,
+    "body" .= body
+    ]
+messagePairs (Link title body url) = [
+    "type" .= ("link" :: T.Text),
+    "title" .= title,
+    "body" .= body,
+    "url" .= url
+    ]
+
+instance ToJSON Message where
+  toJSON = object . messagePairs
+
+instance FromJSON Message where
+  parseJSON (A.Object v) = do
+    (t :: T.Text) <- v .: "type"
+    case t of
+      "note" -> Note <$> v .:? "title" <*> v .:? "body"
+      "link" -> Link <$> v .:? "title" <*> v .:? "body" <*> v .:? "url"
+      _ -> fail "type should be one of \"note\" or \"link\"."
+  parseJSON invalid = A.typeMismatch "Target" invalid
+
+
+data PushRequest = PushRequest Target Message deriving (Show, Eq, Generic)
+
+instance ToJSON PushRequest where
+  toJSON (PushRequest t m) = object $ targetPairs t <> messagePairs m
+
+instance FromJSON PushRequest where
+  parseJSON v = PushRequest <$> parseJSON v <*> parseJSON v
---
+++ test/Pushbullet/Models/PushRequestSpec.hs
@@ -0,0 +1,37 @@
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module Pushbullet.Models.PushRequestSpec where
+
+import           Control.Applicative           ((<$>), (<*>))
+import           Data.Aeson                    (decode, encode)
+import           Pushbullet.Models.PushRequest
+import           Test.Hspec
+import           Test.QuickCheck
+import           Test.QuickCheck.Instances
+
+instance Arbitrary Target where
+  arbitrary = oneof [
+      pure All
+    , Device <$> arbitrary
+    , Email <$> arbitrary
+    , Channel <$> arbitrary
+    , Client <$> arbitrary
+    ]
+  shrink = genericShrink
+
+instance Arbitrary Message where
+  arbitrary = oneof [
+      Note <$> arbitrary <*> arbitrary
+    , Link <$> arbitrary <*> arbitrary <*> arbitrary
+    ]
+  shrink = genericShrink
+
+instance Arbitrary PushRequest where
+  arbitrary = PushRequest <$> arbitrary <*> arbitrary
+  shrink = genericShrink
+
+spec :: Spec
+spec = do
+  describe "PushRequest" $ do
+    it "decode . encode should roundtrip" $ property $ \(p :: PushRequest) -> do
+      (decode . encode) p `shouldBe` Just p
