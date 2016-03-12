7eb5948b3194153a44b253d842d205d1f10f1aec
Josh Bohde <josh@joshbohde.com>
2016-03-12 13:15:28 -0600

    Add a direct encoding of the api objects.

--- src/Pushbullet/Models/Push.hs
+++ src/Pushbullet/Models/Push.hs
@@ -1,15 +1,20 @@
 {-# LANGUAGE TemplateHaskell #-}
 module Pushbullet.Models.Push (
+  Push(..),
+  Pushes(..),
   PushType(..),
+  PushRequest(..),
+  PushUpdate(..),
   Direction(..),
-  Push(..)
+  User(..)
   ) where
 
-import           Data.Aeson.TH (constructorTagModifier, defaultOptions,
-                                deriveJSON, fieldLabelModifier)
-import           Data.Char     (toLower)
-import qualified Data.Text     as T
-import           Data.Vector   (Vector)
+import           Data.Aeson.Casing (aesonDrop, snakeCase)
+import           Data.Aeson.TH     (constructorTagModifier, defaultOptions,
+                                    deriveJSON)
+import           Data.Char         (toLower)
+import qualified Data.Text         as T
+import           Data.Vector       (Vector)
 
 data PushType = Note | File | Link deriving (Eq, Show)
 $(deriveJSON defaultOptions{constructorTagModifier = map toLower} ''PushType)
@@ -18,35 +23,81 @@ data Direction = Self | Outgoing | Incoming deriving (Show, Eq)
 $(deriveJSON defaultOptions{constructorTagModifier = map toLower} ''Direction)
 
 data Push = Push {
-  iden                      :: T.Text,
-  active                    :: Bool,
-  created                   :: Float,
-  modified                  :: Float,
-  push_type                 :: PushType,
-  dismissed                 :: Bool,
-  guid                      :: Maybe T.Text,
-  direction                 :: Direction,
-  sender_iden               :: T.Text,
-  sender_email              :: T.Text,
-  sender_email_normalized   :: T.Text,
-  sender_name               :: T.Text,
-  receiver_iden             :: T.Text,
-  receiver_email            :: T.Text,
-  receiver_email_normalized :: T.Text,
-  target_device_iden        :: Maybe T.Text,
-  source_device_iden        :: Maybe T.Text,
-  client_iden               :: Maybe T.Text,
-  channel_iden              :: Maybe T.Text,
-  awake_app_guids           :: Maybe (Vector T.Text),
-  title                     :: Maybe T.Text,
-  body                      :: Maybe T.Text,
-  url                       :: Maybe T.Text,
-  file_name                 :: Maybe T.Text,
-  file_type                 :: Maybe T.Text,
-  file_url                  :: Maybe T.Text,
-  image_url                 :: Maybe T.Text,
-  image_width               :: Maybe Int,
-  image_height              :: Maybe Int
+  pushIden                    :: T.Text,
+  pushActive                  :: Bool,
+  pushCreated                 :: Float,
+  pushModified                :: Float,
+  pushType                    :: PushType,
+  pushDismissed               :: Bool,
+  pushGuid                    :: Maybe T.Text,
+  pushDirection               :: Direction,
+  pushSenderIden              :: T.Text,
+  pushSenderEmail             :: T.Text,
+  pushSenderEmailNormalized   :: T.Text,
+  pushSenderName              :: T.Text,
+  pushReceiverIden            :: T.Text,
+  pushReceiverEmail           :: T.Text,
+  pushReceiverEmailNormalized :: T.Text,
+  pushTargetDeviceIden        :: Maybe T.Text,
+  pushSourceDeviceIden        :: Maybe T.Text,
+  pushClientIden              :: Maybe T.Text,
+  pushChannelIden             :: Maybe T.Text,
+  pushAwakeAppGuids           :: Maybe (Vector T.Text),
+  pushTitle                   :: Maybe T.Text,
+  pushBody                    :: Maybe T.Text,
+  pushUrl                     :: Maybe T.Text,
+  pushFileName                :: Maybe T.Text,
+  pushFileType                :: Maybe T.Text,
+  pushFileUrl                 :: Maybe T.Text,
+  pushImageUrl                :: Maybe T.Text,
+  pushImageWidth              :: Maybe Int,
+  pushImageHeight             :: Maybe Int
   } deriving (Show, Eq)
 
-$(deriveJSON defaultOptions{fieldLabelModifier = (\n -> if n == "push_type" then "type" else n)} ''Push)
+$(deriveJSON (aesonDrop 4 snakeCase) ''Push)
+
+data PushRequest = PushRequest {
+  pushRequestType             :: PushType,
+  pushRequestTitle            :: Maybe T.Text,
+  pushRequestBody             :: Maybe T.Text,
+  pushRequestUrl              :: Maybe T.Text,
+  pushRequestFileName         :: Maybe T.Text,
+  pushRequestFileType         :: Maybe T.Text,
+  pushRequestFileUrl          :: Maybe T.Text,
+  pushRequestSourceDeviceIden :: Maybe T.Text,
+  pushRequestDeviceIden       :: Maybe T.Text,
+  pushRequestClientIden       :: Maybe T.Text,
+  pushRequestChannelTag       :: Maybe T.Text,
+  pushRequestEmail            :: Maybe T.Text,
+  pushRequestGuid             :: Maybe T.Text
+  } deriving (Show, Eq)
+
+$(deriveJSON (aesonDrop 11 snakeCase) ''PushRequest)
+
+data Pushes = Pushes {
+  pushes :: Vector Push
+  } deriving (Show, Eq)
+
+$(deriveJSON defaultOptions ''Pushes)
+
+data PushUpdate = PushUpdate {
+  dismissed :: Bool
+  } deriving (Show, Eq)
+
+$(deriveJSON defaultOptions ''PushUpdate)
+
+
+data User = User {
+  userIden            :: T.Text,
+  userCreated         :: Float,
+  userModified        :: Float,
+  userEmail           :: T.Text,
+  userEmailNormalized :: T.Text,
+  userName            :: T.Text,
+  userImageUrl        :: T.Text,
+  userMaxUploadSize   :: Int,
+  userReferredCount   :: Maybe Int,
+  userReferrerIden    :: Maybe T.Text
+  } deriving (Show, Eq)
+
+$(deriveJSON (aesonDrop 4 snakeCase) ''User)
